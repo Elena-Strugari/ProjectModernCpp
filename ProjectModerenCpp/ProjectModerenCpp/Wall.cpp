8bc49e86861286e9ed49cea04ffe3deffec6308b
Scarlat Alexandra Nicoleta <nicoleta.scarlat@student.unitbv.ro>
2024-10-31 14:49:05 +0200

    I added basic functions in class Wall.

--- ProjectModerenCpp/ProjectModerenCpp/Wall.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/Wall.cpp
@@ -1,1 +1,30 @@
 #include "Wall.h"
+
+Wall::Wall(uint16_t x, uint16_t y, bool destructible, bool exist, uint8_t hitCount /* Bomb* bomb, Heart* heart*/)
+	: m_position{ x,y }
+	, m_destructible(destructible)
+	, m_exist(true)
+	, m_hitCount(hitCount)
+{
+	//EMPTY
+}
+
+bool Wall::isDistructible() const
+{
+	return m_destructible;
+}
+
+Position Wall::getPosition() const
+{
+	return m_position;
+}
+
+bool Wall::destroy() const
+{
+	return false;
+}
+
+bool Wall::hit()
+{
+	return false;
+}
--- ProjectModerenCpp/ProjectModerenCpp/Wall.h
+++ ProjectModerenCpp/ProjectModerenCpp/Wall.h
@@ -11,7 +11,15 @@ class Wall
 {
 public:
 
-	
+	//CONSTRUCTOR
+	Wall(uint16_t x, uint16_t y, bool destructible, bool exist, uint8_t hitCount /* Bomb* bomb, Heart* heart*/);
+
+	bool isDistructible()const;
+	Position getPosition()const;
+	bool destroy()const;
+	bool hit();
+	//GETTERS
+
 
 
 private:
