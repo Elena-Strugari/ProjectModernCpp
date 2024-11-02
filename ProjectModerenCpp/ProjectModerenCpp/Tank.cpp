24b04ea80e331185be1b5e3c41e67d53c6dab96d
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-11-02 23:19:16 +0200

    I added getters and setters for the tank class

---
+++ ProjectModerenCpp/ProjectModerenCpp/Tank.cpp
@@ -0,0 +1,28 @@
+#include "Tank.h"
+#include <iostream>
+
+Tank::Tank(uint16_t startX, uint16_t startY, Type type)
+    : m_position{ startX, startY }
+    , m_direction{ Direction::Up }
+    , m_health{ 100 }
+    , m_speed{ 1.0f }
+    , m_type{ type }
+{
+    // empty
+}
+
+void Tank::setPosition(int x, int y) {
+    m_position.x = x;
+    m_position.y = y;
+}
+Position Tank::getPosition() const {
+    return m_position;
+}
+
+Tank::Direction Tank::getDirection() const {
+    return m_direction;
+}
+
+int Tank::getHealth() const {
+    return m_health;
+}
--- ProjectModerenCpp/ProjectModerenCpp/Tank.h
+++ ProjectModerenCpp/ProjectModerenCpp/Tank.h
@@ -1,1 +1,38 @@
 #pragma once
+#include <cstdint>
+#include "Bomb.h"
+#include "Wall.h"
+
+class Tank {
+public:
+    enum class Direction : uint8_t {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    enum class Type : uint8_t {
+        Player,
+        Enemy
+    };
+
+    Tank(uint16_t startX, uint16_t startY, Type type);
+
+    void move(Direction direction);
+    void rotate(Direction newDirection);
+    // void shoot();
+    void takeDamage(int amount);
+    bool isDestroyed() const;
+    void setPosition(int x, int y);
+    Position getPosition() const;
+    Direction getDirection() const;
+    int getHealth() const;
+
+private:
+    Position m_position;
+    Direction m_direction;
+    int m_health;
+    float m_speed;
+    Type m_type;
+};
