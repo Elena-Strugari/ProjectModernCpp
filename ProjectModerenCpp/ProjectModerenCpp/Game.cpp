69bf820936b6bca22a11bd21d117e6aa67e92cdf
elena S <strugarielenaral@gmail.com>
2024-11-02 08:01:17 +0200

    create Game class

---
+++ ProjectModerenCpp/ProjectModerenCpp/Game.cpp
@@ -0,0 +1,17 @@
+#include "Game.h"
+
+void Game::start()
+{
+}
+
+void Game::update()
+{
+}
+
+void Game::checkCollisions()
+{
+}
+
+void Game::HandleInput()
+{
+}
---
+++ ProjectModerenCpp/ProjectModerenCpp/Game.h
@@ -0,0 +1,16 @@
+#pragma once
+class Game
+{
+public:
+	void start();
+	void update();
+	void checkCollisions();
+	void HandleInput();
+
+private:
+	/*Map  map;
+	std::vector<Tank> tanks;
+	std::vector<Bullet> bullets;
+	Player player;*/
+};
+
