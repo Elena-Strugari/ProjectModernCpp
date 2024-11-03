218cf1bc3a1cfd820b1e74ac3d37912eb0cc6791
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-11-03 23:51:22 +0200

    I created the tank class and the constructor

---
+++ ProjectModerenCpp/ProjectModerenCpp/Tank.cpp
@@ -0,0 +1,12 @@
+#include "Tank.h"
+#include <iostream>
+
+Tank::Tank(uint16_t startX, uint16_t startY, Type type)
+    : m_position{ startX, startY }
+    , m_direction{ Direction::Up }
+    , m_health{ 100 }
+    , m_speed{ 1.0f }
+    , m_type{ type }
+{
+    // empty
+}
