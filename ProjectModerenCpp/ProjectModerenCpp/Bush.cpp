ff182edb51525953a41454ef1514fb01362453f5
Stoica Andra Stefania <andra.stoica@student.unitbv.ro>
2024-11-07 13:35:02 +0200

    try to resolv all errors from smart git

--- ProjectModerenCpp/ProjectModerenCpp/Bush.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/Bush.cpp
@@ -1,6 +1,21 @@
 #include "Bush.h"
 #include"Tank.h"
-void Bush::GoThrough(Position m_bushPsition, Tank player_tank)
+#include<thread>
+#include<chrono>
+void Bush::GoThrough(Tank &player_tank)
 {
+	float InitialSpeed = player_tank.GetSpeed(); //  nu ma ajuta o functide de get pentru ca eu trebuie sa modific viteza (care e private)
+	/*
+	 variable = newValue;
+    std::cout << "Valoare temporară: " << variable << std::endl;
 
+    // Creează un thread care așteaptă perioada specificată și apoi revine la valoarea inițială
+    std::thread([&variable, initialValue, duration]() {
+        std::this_thread::sleep_for(duration);
+        variable = initialValue;
+        std::cout << "Valoare restaurată: " << variable << std::endl;
+    }).detach();
+
+    cod de pe chatgbt; doar ca sintaxa; trebuie adaptat
+	*/
 }
