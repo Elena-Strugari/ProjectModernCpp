debe1d2040f4de7b4a71cd29857b67902e980d1f
Stoica Andra Stefania <andra.stoica@student.unitbv.ro>
2024-11-07 11:01:56 +0200

    bonuslife.cpp

--- ProjectModerenCpp/ProjectModerenCpp/BonusLife.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/BonusLife.cpp
@@ -3,7 +3,8 @@
 #include"Player.h"
 void BonusLife::Collect(Position m_heartPosition)
 {
-	//creste viata jucatorului cu 1; coordonatele bonusului folosit devin valori invalide(ex: -1) dupa ce a fost "colectat"
+	//creste viata jucatorului cu 1; 
+	// coordonatele bonusului folosit devin valori invalide(ex: -1) dupa ce a fost "colectat"
 	lives++;
 	m_heartPosition.x = -1;
 	m_heartPosition.y = -1;
