ea35c5e8293125d4064e7d6790aa939092b8f297
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-11-17 21:44:34 +0200

    I added the necessary changes in the header for the tank's movements

--- ProjectModerenCpp/ProjectModerenCpp/MoveObject.h
+++ ProjectModerenCpp/ProjectModerenCpp/MoveObject.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GameObject.h"
+#include "Map.h"
 
 class MoveObject: public GameObject
 {
@@ -19,10 +20,15 @@ public:
         Base
     };
 public:
-    MoveObject(GameObject&& obj);
+    MoveObject(GameObject&& obj, Map* map, int startX = 0, int startY = 0);
     void ShootBullet();
+    void Move(Direction direction);
+    void HandleInput();
 
 private:
     Direction m_direction;
+    Bullet m_bullet;
+    int x, y;
+    Map* m_map;
 };
 
