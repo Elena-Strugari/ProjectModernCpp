1f56edc41f5adeebba569132199981156910a5b6
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-11-17 22:00:09 +0200

    I created the Handle Input function to associate the movement of the tank with the keyboard arrows

--- ProjectModerenCpp/ProjectModerenCpp/MoveObject.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/MoveObject.cpp
@@ -45,3 +45,72 @@ void MoveObject::Move(Direction direction) {
         std::cout << "Mutarea nu este posibila: obstacol sau iesire din harta\n";
     }
 }
+void MoveObject::HandleInput() {
+    std::cout << "Folositi sagetile pentru a muta tancul" << std::endl << "'b' pentru a trage, 'q' pentru a iesi\n";
+
+    char key;
+    while (true) {
+        if (_kbhit()) {
+            key = _getch();
+            if (key == 'q') {
+                std::cout << "Joc încheiat.\n";
+                break;
+            }
+            if (key == 'b') {
+                ShootBullet();
+            }
+            if (key == 72) {
+                Move(Direction::Up);
+                std::cout << "Tancul s-a deplasat in sus.\n";
+            }
+            if (key == 80) {
+                Move(Direction::Down);
+                std::cout << "Tancul s-a deplasat in jos.\n";
+            }
+            if (key == 75) {
+                Move(Direction::Left);
+                std::cout << "Tancul s-a deplasat la stanga.\n";
+            }
+            if (key == 77) {
+                Move(Direction::Right);
+                std::cout << "Tancul s-a deplasat la dreapta.\n";
+            }
+
+            m_map->PrintWithBorder();
+        }
+    }
+}
+
+//void move(Direction direction);
+       //void rotate(Direction newDirection);
+       //// void shoot();
+       //void takeDamage(int amount);
+       //bool isDestroyed() const;
+       //void setPosition(int x, int y);
+       //Position getPosition() const;
+       //Direction getDirection() const;
+       //uint16_t getHealth() const;
+       //float GetSpeed()const;
+
+
+
+       //void Tank::setPosition(int x, int y) {
+       //    m_position.x = x;
+       //    m_position.y = y;
+       //}
+       //Position Tank::getPosition() const {
+       //    return m_position;
+       //}
+       //
+       //Tank::Direction Tank::getDirection() const {
+       //    return m_direction;
+       //}
+       //
+       //uint16_t Tank::getHealth() const {
+       //    return m_health;
+       //}
+       //
+       //float Tank::GetSpeed() const
+       //{
+       //    return m_speed;
+       //
