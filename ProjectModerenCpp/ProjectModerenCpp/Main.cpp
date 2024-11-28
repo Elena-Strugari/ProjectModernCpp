84f11faf2eddf67b78ebd4573375730b9662dafc
elena S <strugarielenaral@gmail.com>
2024-11-28 15:38:40 +0200

    to change green tree

--- ProjectModerenCpp/ProjectModerenCpp/Main.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/Main.cpp
@@ -8,6 +8,7 @@
 #include "MoveObject.h"
 int main()
 {
+<<<<<<< Updated upstream
 	Map matrix(1);
 	matrix.GenerateMapElements(matrix);
 	//matrix.print();
@@ -16,13 +17,23 @@ int main()
 	std::vector<std::pair<uint16_t, uint16_t>>startPosition = p.PlayersStartPositions(matrix.GetWidth(), matrix.GetHeight());
 	for (int i = 0; i < 4; i++)
 		std::cout << "Playier " << i << " : " << startPosition[i].first << "," << startPosition[i].second << "\n";
+=======
+	Map matrix(2);
+	matrix.GenerateWalls(2);
+	//matrix.print();
+	matrix.PrintWithBorder();
+	//Position p;
+	//std::vector<std::pair<uint16_t, uint16_t>>startPosition = p.PlayersStartPositions(matrix.GetWidth(), matrix.GetHeight());
+	//for (int i = 0; i < 4; i++)
+	//	std::cout << "Playier " << i << " : " << startPosition[i].first << "," << startPosition[i].second << "\n";
+>>>>>>> Stashed changes
 
 
-	//afis 
-	GameObject obj(Tank::ColorTank::Blue);
-	MoveObject tank(std::move(obj), &matrix, startPosition[0].first, startPosition[0].second);
-	//tank.ShootBullet();
-    tank.HandleInput();
+	////afis 
+	//GameObject obj(Tank::ColorTank::Blue);
+	//MoveObject tank(std::move(obj), &matrix, startPosition[0].first, startPosition[0].second);
+	////tank.ShootBullet();
+ //   tank.HandleInput();
 
 	return 0;
 }
