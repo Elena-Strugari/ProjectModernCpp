d9d570cabf8f25fd90df980f08bb321f7e2ff673
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-11-28 16:58:18 +0200

    Game and Move

--- ProjectModerenCpp/ProjectModerenCpp/GameObject.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/GameObject.cpp
@@ -1,14 +1,33 @@
-﻿//#include "GameObject.h"
-//
-//
-//GameObject::GameObject(Tank::ColorTank tank)
-//	:m_GOTank(tank)
-//{
-//
-//}
-//
-//std::ostream& operator<<(std::ostream& os, const GameObject& go)
-//{
-//	os << "GameObject: " << go.m_GOTank << " and Bullet: " << go.m_GOBullet;
-//	return os;
-//}
+﻿#include "GameObject.h"
+
+
+GameObject::GameObject()
+{
+	std::cout << "costructor def gameObject";
+}
+
+GameObject::GameObject(Tank::ColorTank tank)
+	:m_GOTank(tank) {}
+
+const Tank& GameObject::GetTank() const
+{
+	return m_GOTank;
+}
+
+const Bullet& GameObject::GetBullet() const
+{
+	return m_GOBullet;
+}
+
+void GameObject::ShootObj()
+{
+	m_GOBullet.Shoot();
+}
+
+std::ostream& operator<<(std::ostream& os, const GameObject& go)
+{
+	os << "GameObject: " << go.m_GOTank << " and Bullet: " << go.m_GOBullet;
+	return os;
+}
+
+//denisa
--- ProjectModerenCpp/ProjectModerenCpp/MoveObject.h
+++ ProjectModerenCpp/ProjectModerenCpp/MoveObject.h
@@ -1,37 +1,92 @@
-////client
-//#pragma once
-//#include "GameObject.h"
-//#include "Map.h"
-//#include <cstdint>
-//
-//class MoveObject: public GameObject
-//{
-//public:
-//    enum class Direction {
-//        Up,
-//        Down,
-//        Left,
-//        Right
-//    };
-//
-//    enum class CollisionType {
-//        None,
-//        Wall,
-//        Tank,
-//        Bullet,
-//        Base
-//    };
-//public:
-//    MoveObject(GameObject&& obj, Map* map, uint16_t startX = 0, uint16_t startY = 0);
-//    void ShootBullet();
-//    void Move(Direction direction);
-//    void HandleInput();
-//    void MoveCursorToTop();
-//
-//private:
-//    Direction m_direction;
-//    Bullet m_bullet;
-//    uint16_t m_MOx, m_MOy;
-//    Map* m_map;
-//};
-//
+﻿#pragma once
+#include "GameObject.h"
+#include "Map.h"
+#include <cstdint>
+
+class MoveObject : public GameObject
+{
+public:
+    enum class Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    enum class CollisionType {
+        None,
+        Wall,
+        Tank,
+        Bullet,
+        Base
+    };
+public:
+    MoveObject() = default;
+    MoveObject(GameObject&& obj, Map* map);
+    MoveObject(const MoveObject&) = delete;    // Explicitly deleted copy constructor
+    ~MoveObject() = default;
+    MoveObject& operator=(MoveObject&& other) noexcept {
+        if (this == &other) // Verificăm auto-atribuirea
+            return *this;
+        m_MOx = other.m_MOx;
+        m_MOy = other.m_MOy;
+        m_color = other.m_color;
+        return *this;
+    }
+
+    //Get 
+    uint16_t GetX()const;
+    uint16_t GetY()const;
+    std::pair<uint16_t, uint16_t> GetPosition()const;
+
+
+    //set 
+    void SetPosition(uint16_t x, uint16_t y);
+    void SetColor(Tank::ColorTank color);
+
+    //valid 
+    bool IsValidPos(uint16_t x, uint16_t y)const;
+
+    void ShootBullet();
+    void Move(Direction direction);
+    //std::tuple<uint16_t, uint16_t, Tank::ColorTank>  MoveServer2(Direction direction);
+    void MoveServer(uint16_t x, uint16_t y, Direction direction);
+    void HandleInput();
+    void MoveCursorToTop();
+    std::tuple<uint16_t, uint16_t, Tank::ColorTank> StartPositionColor();
+
+private:
+    Direction m_direction;
+    Bullet m_bullet;
+    uint16_t m_MOx, m_MOy;
+    Tank::ColorTank m_color;
+    Map* m_map;
+};
+
+
+//idee
+
+/*
+//private
+
+CollisionType CheckCollision(int x, int y) {
+        // Poți implementa logica de coliziune
+        // De exemplu, verifică dacă poziția (x, y) este pe un perete sau în afacerea unei alte entități
+        if (m_map->IsWall(x, y)) {
+            return CollisionType::Wall;
+        }
+
+        // Dacă există alt obiect (tank, bază, etc.) pe acea poziție
+        if (m_map->IsTankAt(x, y)) {
+            return CollisionType::Tank;
+        }
+
+        // Dacă există un proiectil pe acea poziție
+        if (m_map->IsBulletAt(x, y)) {
+            return CollisionType::Bullet;
+        }
+
+        return CollisionType::None;
+    }
+
+*/
