a4cb614ab86c635f84e71921e1cd3cb2b4939a03
elena S <strugarielenaral@gmail.com>
2024-11-28 17:04:56 +0200

    final push today

--- ProjectModerenCpp/ProjectModerenCpp/Main.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/Main.cpp
@@ -1,39 +1,23 @@
-﻿//server 
-#include <iostream>
+﻿#include <iostream>
 #include "Map.h"
-#include "UserManager.h"
 #include<utility>
 #include<vector>
-#include"Position.h"
-#include "MoveObject.h"
+#include "Tank.h"
+
+#include "routing.h"
+
+using namespace http;
 int main()
 {
-<<<<<<< Updated upstream
-	Map matrix(1);
-	matrix.GenerateMapElements(matrix);
-	//matrix.print();
-	//matrix.PrintWithBorder();
-	Position p;
-	std::vector<std::pair<uint16_t, uint16_t>>startPosition = p.PlayersStartPositions(matrix.GetWidth(), matrix.GetHeight());
-	for (int i = 0; i < 4; i++)
-		std::cout << "Playier " << i << " : " << startPosition[i].first << "," << startPosition[i].second << "\n";
-=======
-	Map matrix(2);
-	matrix.GenerateWalls(2);
-	//matrix.print();
-	matrix.PrintWithBorder();
-	//Position p;
-	//std::vector<std::pair<uint16_t, uint16_t>>startPosition = p.PlayersStartPositions(matrix.GetWidth(), matrix.GetHeight());
-	//for (int i = 0; i < 4; i++)
-	//	std::cout << "Playier " << i << " : " << startPosition[i].first << "," << startPosition[i].second << "\n";
->>>>>>> Stashed changes
+	/*Map map(3);
+	map.GenerateWalls(3);
+	map.DisplayMap();*/
 
+	//Tank t(Tank::ColorTank::Blue);
+	//std::cout<<t.GetColor();
+	Routing r;
+	r.Run();
 
-	////afis 
-	//GameObject obj(Tank::ColorTank::Blue);
-	//MoveObject tank(std::move(obj), &matrix, startPosition[0].first, startPosition[0].second);
-	////tank.ShootBullet();
- //   tank.HandleInput();
 
 	return 0;
 }
