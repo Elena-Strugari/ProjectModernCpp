011c2a5620974781e93daac9fba033054ffe5d6b
Stoica Andra Stefania <andra.stoica@student.unitbv.ro>
2024-11-28 16:37:06 +0200

    map.h

--- ProjectModerenCpp/ProjectModerenCpp/Map.h
+++ ProjectModerenCpp/ProjectModerenCpp/Map.h
@@ -1,33 +1,56 @@
-////de discutat, cred server
-//#pragma once
-//#include <cstdint>
-//#include <iostream>
-//#include <vector>
-//#include "Wall.h"
-//
-//class Map
-//{
-//public:
-//	//CONSTRUCTOR
-//	Map(uint8_t level); 
-//	void Print()const;     
-//	void PrintWithBorder()const;
-//	bool IsValidPosition(uint16_t x, uint16_t y) const;
-//	void SetCell(uint16_t x, uint16_t y, char value);
-//	char GetCell(uint16_t x, uint16_t y) const;
-//	void AddWall(uint16_t x, uint16_t y, Wall::Destructible destructible);
-//	void AddBomb(uint16_t x, uint16_t y);
-//	void GenerateMapElements(Map& map);
-//	void AddBonusLife(uint16_t x, uint16_t y);
-//	uint16_t GetWidth()const;
-//	uint16_t GetHeight()const;
-//private:
-//
-//	uint16_t m_width;
-//	uint16_t m_height;
-//	uint16_t m_level;
-//	std::vector<std::vector<char>> m_map;
-//	std::vector<std::pair<uint16_t, uint16_t>>RandomWall(uint16_t x, uint16_t y,int NumberOfWalls);
-//	//std::vector<std::vector<Wall>> m_grid;
-//};
-//
+#pragma once
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include "Wall.h"
+
+class Map
+{
+public:
+	struct Cell {
+		Wall::TypeWall typeWall;
+		uint16_t space;
+		uint16_t border;
+		//bomba 
+
+	};
+public:
+
+	Map();
+	Map(uint8_t level);
+
+	//Get
+	uint16_t GetWidth() const;
+	uint16_t GetHeight() const;
+	Cell GetType()const;
+	std::vector<std::vector<Map::Cell>>GetMap()const;
+	Map::Cell GetCell(uint16_t x, uint16_t y) const;
+
+	//set
+	void SetCell(uint16_t x, uint16_t y, Cell value);
+
+	//validation
+	bool IsValidPosition(uint16_t x, uint16_t y) const;
+
+	//display 
+	void DisplayMap()const;
+
+	//Add
+	void GenerateWalls(uint8_t level);
+	void AddWall(uint16_t x, uint16_t y, Wall::TypeWall destructible);
+
+
+
+
+private:
+	uint16_t m_width;
+	uint16_t m_height;
+	std::vector<Wall> m_wall;
+	std::vector<std::vector<Cell>> m_map;
+
+	Cell m_cellType;
+
+	std::pair<uint16_t, uint16_t> getLevelBounds(uint8_t level) const;
+	std::vector<std::pair<uint16_t, uint16_t>>RandomWall(uint16_t x, uint16_t y, int numberOfWalls);
+
+};
