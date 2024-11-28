dbcfe27d42327ec1dbd2c3c38d83354bbe91ca67
elena S <strugarielenaral@gmail.com>
2024-11-28 16:09:28 +0200

    Server Controller class

---
+++ ProjectModerenCpp/ProjectModerenCpp/ServerController.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <crow.h>
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <conio.h> // Pentru _kbhit() și _getch()
+
+class ServerController {
+public:
+    ServerController(crow::SimpleApp& app);
+    void StartServer(); // Pornire server
+    void StopServer();  // Oprire server
+    void MonitorInput(); // Monitorizare tastatură (pentru ESC)
+    void Run();
+private:
+    crow::SimpleApp& m_app;
+    std::thread m_serverThread;
+    std::atomic<bool> m_running{ true }; // Flag pentru a controla serverul
+};
