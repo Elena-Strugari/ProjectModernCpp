e1bbaae332a1a6bc1f881a3f65d0b40ae34cdc39
Scarlat Alexandra Nicoleta <nicoleta.scarlat@student.unitbv.ro>
2024-11-28 16:51:04 +0200

    Generate walls function.

--- ProjectModerenCpp/ProjectModerenCpp/Map.cpp
+++ ProjectModerenCpp/ProjectModerenCpp/Map.cpp
@@ -133,3 +133,115 @@ std::vector<std::pair<uint16_t, uint16_t>> Map::RandomWall(uint16_t width, uint1
 
 }
 
+void Map::GenerateWalls(uint8_t level)
+{
+    uint16_t height = GetHeight();
+    uint16_t width = GetWidth();
+
+    // A helper function to add a list of walls
+    auto addWalls = [this](const std::vector<std::pair<uint16_t, uint16_t>>& coordinates, Wall::TypeWall type) {
+        for (const auto& coord : coordinates) {
+            AddWall(coord.first, coord.second, type);
+        }
+        };
+
+    // A helper function to generate random walls
+    auto generateRandomWalls = [this, width, height](int numWalls) {
+        return RandomWall(width, height, numWalls);
+        };
+
+    switch (level)
+    {
+    case 1: {
+        // Define fixed indestructible walls for level 1
+        std::vector<std::pair<uint16_t, uint16_t>> indestructibleWalls = {
+            {0, 6}, {1, 6}, {1, 7}, {3, 8}, {4, 0}, {4, 1}, {4, 8},
+            {5, 2}, {5, 8}, {5, 9}, {5, 10}, {5, 14}, {6, 6}, {6, 14},
+            {7, 5}, {7, 6}, {7, 7}, {10, 0}, {10, 1}, {10, 2}, {10, 3},
+            {10, 11}, {10, 12}, {12, 5}, {12, 9}, {12, 10}, {12, 11},
+            {12, 12}, {13, 5}, {13, 12}, {14, 5}, {12, 13}, {13, 14},
+            {14, 13}, {9, 5}, {8, 6}, {7, 8}, {6, 9}, {5, 11}, {4, 12},
+            {3, 13}, {2, 14}
+        };
+        addWalls(indestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Random indestructible walls
+        auto randomIndestructibleWalls = generateRandomWalls(15);
+        addWalls(randomIndestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Destructible walls
+        std::vector<std::pair<uint16_t, uint16_t>> destructibleWalls = {
+            {2, 2}, {2, 6}, {2, 9}, {6, 9}, {8, 8}, {11, 4}, {11, 9}, {13, 7}, {14, 10}
+        };
+        addWalls(destructibleWalls, Wall::TypeWall::destructible);
+
+        // Random destructible walls
+        auto randomDestructibleWalls = generateRandomWalls(40);
+        addWalls(randomDestructibleWalls, Wall::TypeWall::destructible);
+
+        break;
+    }
+
+    case 2: {
+        // Define fixed indestructible walls for level 2
+        std::vector<std::pair<uint16_t, uint16_t>> indestructibleWalls = {
+            {2, 1}, {2, 2}, {2, 3}, {0, 7}, {0, 8}, {0, 9}, {1, 8}, {2, 8}, {3, 8},
+            {4, 12}, {1, 16}, {2, 16}, {6, 6}, {6, 7}, {6, 8}, {6, 9}, {7, 13}, {8, 13},
+            {9, 13}, {10, 11}, {10, 12}, {9, 19}, {7, 19}, {8, 19}, {8, 18}, {9, 4},
+            {10, 4}, {11, 4}, {13, 5}, {11, 1}, {11, 2}, {11, 3}, {11, 4}, {11, 7},
+            {12, 7}, {13, 7}, {14, 7}, {11, 4}, {14, 8}, {14, 9}, {15, 2}, {18, 4},
+            {19, 4}, {17, 12}, {17, 13}, {17, 14}, {16, 13}, {18, 14}, {15, 18}
+        };
+        addWalls(indestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Random indestructible walls
+        auto randomIndestructibleWalls = generateRandomWalls(15);
+        addWalls(randomIndestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Random destructible walls
+        auto randomDestructibleWalls = generateRandomWalls(50);
+        addWalls(randomDestructibleWalls, Wall::TypeWall::destructible);
+
+        break;
+    }
+    case 3: {
+        // Define fixed indestructible walls for level 3
+
+        std::vector<std::pair<uint16_t, uint16_t>> indestructibleWalls = {
+            {0, 2}, {0, 3}, {0, 4}, {2, 9}, {2, 10}, {2, 19}, {2, 20}, {2, 21},
+            {2, 22}, {2, 23}, {2, 24}, {3, 9}, {3, 10}, {3, 24}, {4, 11}, {4, 12},
+            {4, 24}, {5, 24}, {6, 24}, {7, 24}, {8, 3}, {8, 4}, {8, 24}, {9, 4},
+            {10, 4}, {10, 11}, {10, 12}, {10, 13}, {10, 14}, {11, 7}, {11, 10},
+            {11, 12}, {12, 5}, {12, 6}, {12, 12}, {13, 4}, {13, 5}, {13, 6}, {13, 12},
+            {14, 12}, {14, 19}, {15, 1}, {15, 19}, {16, 1}, {16, 19}, {17, 1},
+            {17, 18}, {17, 19}, {18, 1}, {18, 19}, {18, 20}, {18, 21}, {19, 1},
+            {19, 13}, {19, 21}, {19, 22}, {20, 8}, {21, 3}, {21, 12}, {21, 13},
+            {21, 14}, {22, 8}, {22, 14}, {22, 15}, {22, 16}, {23, 8}, {23, 16},
+            {24, 8}, {24, 16}
+        };
+        addWalls(indestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Random indestructible walls
+        auto randomIndestructibleWalls = generateRandomWalls(15);
+        addWalls(randomIndestructibleWalls, Wall::TypeWall::indestructible);
+
+        // Destructible walls
+        std::vector<std::pair<uint16_t, uint16_t>> destructibleWalls = {
+            {1, 9}, {2, 8}, {3, 7}, {3, 8}, {3, 20}, {3, 21}, {3, 22}, {3, 23},
+            {4, 6}, {4, 7}, {5, 6}, {5, 7}, {7, 17}, {7, 18}, {8, 17}, {8, 18},
+            {9, 17}, {9, 18}, {11, 4}, {12, 4}, {15, 12}, {16, 12}, {17, 12},
+            {17, 13}
+        };
+        addWalls(destructibleWalls, Wall::TypeWall::destructible);
+
+        // Random destructible walls
+        auto randomDestructibleWalls = generateRandomWalls(40);
+        addWalls(randomDestructibleWalls, Wall::TypeWall::destructible);
+
+        break;
+    }
+
+    default:
+        break;
+    }
+}
