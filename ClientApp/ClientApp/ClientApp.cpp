c215327a06ecde5b92be50e9222d06ca59ea7b1a
elena S <strugarielenaral@gmail.com>
2024-12-04 15:29:35 +0200

    started client with QT

---
+++ ClientApp/ClientApp/ClientApp.cpp
@@ -0,0 +1,109 @@
+﻿#include "ClientApp.h"
+#include <QMessageBox>
+
+ClientApp::ClientApp(QWidget* parent) : QWidget(parent) {
+    QVBoxLayout* layout = new QVBoxLayout(this);
+
+    // Input pentru Client ID și Level
+    clientIdInput = new QLineEdit(this);
+    clientIdInput->setPlaceholderText("Enter Client ID");
+    levelInput = new QLineEdit(this);
+    levelInput->setPlaceholderText("Enter Level (Usor, Mediu, Greu)");
+
+    QPushButton* chooseLevelButton = new QPushButton("Choose Level", this);
+    QPushButton* getMapButton = new QPushButton("Get Map", this);
+
+    QPushButton* moveUpButton = new QPushButton("Move Up", this);
+    QPushButton* moveDownButton = new QPushButton("Move Down", this);
+    QPushButton* moveLeftButton = new QPushButton("Move Left", this);
+    QPushButton* moveRightButton = new QPushButton("Move Right", this);
+
+    outputLabel = new QLabel(this);
+
+    // Adăugăm widget-uri la layout
+    layout->addWidget(clientIdInput);
+    layout->addWidget(levelInput);
+    layout->addWidget(chooseLevelButton);
+    layout->addWidget(getMapButton);
+    layout->addWidget(moveUpButton);
+    layout->addWidget(moveDownButton);
+    layout->addWidget(moveLeftButton);
+    layout->addWidget(moveRightButton);
+    layout->addWidget(outputLabel);
+
+    setLayout(layout);
+
+    // Conectăm butoanele la sloturi
+    connect(chooseLevelButton, &QPushButton::clicked, this, &ClientApp::onChooseLevel);
+    connect(getMapButton, &QPushButton::clicked, this, &ClientApp::onGetMap);
+    connect(moveUpButton, &QPushButton::clicked, this, &ClientApp::onMoveTankUp);
+    connect(moveDownButton, &QPushButton::clicked, this, &ClientApp::onMoveTankDown);
+    connect(moveLeftButton, &QPushButton::clicked, this, &ClientApp::onMoveTankLeft);
+    connect(moveRightButton, &QPushButton::clicked, this, &ClientApp::onMoveTankRight);
+}
+
+void ClientApp::onChooseLevel() {
+    std::string clientId = clientIdInput->text().toStdString();
+    std::string level = levelInput->text().toStdString();
+
+    cpr::Response r = cpr::Post(
+        cpr::Url{ "http://localhost:8080/choose_level" },
+        cpr::Header{ {"Client-ID", clientId} },
+        cpr::Body{ level }
+    );
+
+    if (r.status_code == 200) {
+        outputLabel->setText("Level chosen successfully: " + QString::fromStdString(r.text));
+    }
+    else {
+        outputLabel->setText("Error choosing level: " + QString::fromStdString(r.text));
+    }
+}
+
+void ClientApp::onGetMap() {
+    std::string clientId = clientIdInput->text().toStdString();
+    std::string level = levelInput->text().toStdString();
+
+    cpr::Response r = cpr::Get(
+        cpr::Url{ "http://localhost:8080/get_map" },
+        cpr::Header{ {"Client-ID", clientId} },
+        cpr::Body{ level }
+    );
+
+    if (r.status_code == 200) {
+        outputLabel->setText("Map retrieved successfully! JSON:\n" + QString::fromStdString(r.text));
+    }
+    else {
+        outputLabel->setText("Error retrieving map: " + QString::fromStdString(r.text));
+    }
+}
+
+void ClientApp::sendMoveRequest(const std::string& direction) {
+    std::string clientId = clientIdInput->text().toStdString();
+    std::string url = "http://localhost:8080/move_tank?id=" + clientId + "&direction=" + direction;
+
+    cpr::Response r = cpr::Get(cpr::Url{ url });
+
+    if (r.status_code == 200) {
+        outputLabel->setText("Move successful: " + QString::fromStdString(r.text));
+    }
+    else {
+        outputLabel->setText("Move error: " + QString::fromStdString(r.text));
+    }
+}
+
+void ClientApp::onMoveTankUp() {
+    sendMoveRequest("U");
+}
+
+void ClientApp::onMoveTankDown() {
+    sendMoveRequest("D");
+}
+
+void ClientApp::onMoveTankLeft() {
+    sendMoveRequest("L");
+}
+
+void ClientApp::onMoveTankRight() {
+    sendMoveRequest("R");
+}
