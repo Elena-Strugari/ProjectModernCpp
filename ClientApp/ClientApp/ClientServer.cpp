ced0125d3419d1d789efeba4ecb786132fee6b9b
elena S <strugarielenaral@gmail.com>
2024-12-18 11:16:59 +0200

    create new class for server , change QT

---
+++ ClientApp/ClientApp/ClientServer.cpp
@@ -0,0 +1,148 @@
+﻿//#include "ClientServer.h"
+//
+//
+//void ClientApp::onChooseLevel(const std::string& clientId, const std::string& level) {
+//    // std::string clientId = clientIdInput->text().toStdString();
+//     /*if (clientId.empty()) {
+//         QMessageBox::warning(this, "Error", "Client ID is required!");
+//         return;
+//     }*/
+//
+//    cpr::Response r = cpr::Post(
+//        cpr::Url{ "http://localhost:8080/choose_level" },
+//        cpr::Header{ {"Client-ID", clientId} },
+//        cpr::Body{ level }
+//    );
+//
+//    if (r.status_code == 200) {
+//        outputLabel->setText("Level chosen successfully: " + QString::fromStdString(r.text));
+//    }
+//    else {
+//        outputLabel->setText("Error choosing level: " + QString::fromStdString(r.text));
+//    }
+//}
+//
+//void ClientApp::onChooseSmall() {
+//    onChooseLevel(clientId, "Easy");
+//}
+//
+//void ClientApp::onChooseMedium() {
+//    onChooseLevel("Medium");
+//}
+//
+//void ClientApp::onChooseLarge() {
+//    onChooseLevel("Hard");
+//}
+//
+//
+//
+//#include "ClientApp.h"
+//#include <QMessageBox>
+//
+//ClientApp::ClientApp(QWidget* parent) : QWidget(parent) {
+//    QVBoxLayout* layout = new QVBoxLayout(this);
+//
+//    // Input pentru Client ID și Level
+//    clientIdInput = new QLineEdit(this);
+//    clientIdInput->setPlaceholderText("Enter Client ID");
+//    levelInput = new QLineEdit(this);
+//    levelInput->setPlaceholderText("Enter Level (Usor, Mediu, Greu)");
+//
+//    QPushButton* chooseLevelButton = new QPushButton("Choose Level", this);
+//    QPushButton* getMapButton = new QPushButton("Get Map", this);
+//
+//    QPushButton* moveUpButton = new QPushButton("Move Up", this);
+//    QPushButton* moveDownButton = new QPushButton("Move Down", this);
+//    QPushButton* moveLeftButton = new QPushButton("Move Left", this);
+//    QPushButton* moveRightButton = new QPushButton("Move Right", this);
+//
+//    outputLabel = new QLabel(this);
+//
+//    // Adăugăm widget-uri la layout
+//    layout->addWidget(clientIdInput);
+//    layout->addWidget(levelInput);
+//    layout->addWidget(chooseLevelButton);
+//    layout->addWidget(getMapButton);
+//    layout->addWidget(moveUpButton);
+//    layout->addWidget(moveDownButton);
+//    layout->addWidget(moveLeftButton);
+//    layout->addWidget(moveRightButton);
+//    layout->addWidget(outputLabel);
+//
+//    setLayout(layout);
+//
+//    // Conectăm butoanele la sloturi
+//    connect(chooseLevelButton, &QPushButton::clicked, this, &ClientApp::onChooseLevel);
+//    connect(getMapButton, &QPushButton::clicked, this, &ClientApp::onGetMap);
+//    connect(moveUpButton, &QPushButton::clicked, this, &ClientApp::onMoveTankUp);
+//    connect(moveDownButton, &QPushButton::clicked, this, &ClientApp::onMoveTankDown);
+//    connect(moveLeftButton, &QPushButton::clicked, this, &ClientApp::onMoveTankLeft);
+//    connect(moveRightButton, &QPushButton::clicked, this, &ClientApp::onMoveTankRight);
+//}
+//
+//void ClientApp::onChooseLevel() {
+//    std::string clientId = clientIdInput->text().toStdString();
+//    std::string level = levelInput->text().toStdString();
+//
+//    cpr::Response r = cpr::Post(
+//        cpr::Url{ "http://localhost:8080/choose_level" },
+//        cpr::Header{ {"Client-ID", clientId} },
+//        cpr::Body{ level }
+//    );
+//
+//    if (r.status_code == 200) {
+//        outputLabel->setText("Level chosen successfully: " + QString::fromStdString(r.text));
+//    }
+//    else {
+//        outputLabel->setText("Error choosing level: " + QString::fromStdString(r.text));
+//    }
+//}
+//
+//void ClientApp::onGetMap() {
+//    std::string clientId = clientIdInput->text().toStdString();
+//    std::string level = levelInput->text().toStdString();
+//
+//    cpr::Response r = cpr::Get(
+//        cpr::Url{ "http://localhost:8080/get_map" },
+//        cpr::Header{ {"Client-ID", clientId} },
+//        cpr::Body{ level }
+//    );
+//
+//    if (r.status_code == 200) {
+//        //outputLabel->setText("Map retrieved successfully! JSON:\n" + QString::fromStdString(r.text));
+//        outputLabel->setText("Map retrieved successfully! JSON:\n");
+//    }
+//    else {
+//        outputLabel->setText("Error retrieving map: " + QString::fromStdString(r.text));
+//    }
+//}
+//
+//void ClientApp::sendMoveRequest(const std::string& direction) {
+//    std::string clientId = clientIdInput->text().toStdString();
+//    std::string url = "http://localhost:8080/move_tank/" + direction + "?id=" + clientId;
+//
+//    cpr::Response r = cpr::Get(cpr::Url{ url });
+//
+//    if (r.status_code == 200) {
+//        outputLabel->setText("Move successful: " + QString::fromStdString(r.text));
+//    }
+//    else {
+//        outputLabel->setText("Move error: " + QString::fromStdString(r.text));
+//    }
+//}
+//
+//void ClientApp::onMoveTankUp() {
+//    sendMoveRequest("up");
+//}
+//
+//void ClientApp::onMoveTankDown() {
+//    sendMoveRequest("down");
+//}
+//
+//void ClientApp::onMoveTankLeft() {
+//    sendMoveRequest("left");
+//}
+//
+//void ClientApp::onMoveTankRight() {
+//    sendMoveRequest("right");
+//}
---
+++ ClientApp/ClientApp/ClientServer.h
@@ -0,0 +1,42 @@
+//#pragma once
+//class ClientServer
+//{
+//};
+//
+//#include <QtWidgets/QMainWindow>
+//#include "ui_ClientApp.h"
+//
+//#include <QWidget>
+//#include <QPushButton>
+//#include <QLineEdit>
+//#include <QLabel>
+//#include <QVBoxLayout>
+//#include <cpr/cpr.h>
+//
+////class ClientApp : public QMainWindow
+//class ClientApp : public QWidget
+//{
+//    Q_OBJECT
+//
+//public:
+//
+//    explicit ClientApp(QWidget* parent = nullptr);
+//    // ~ClientApp();
+//
+//private slots:
+//    void onChooseLevel();
+//    void onGetMap();
+//    void onMoveTankUp();
+//    void onMoveTankDown();
+//    void onMoveTankLeft();
+//    void onMoveTankRight();
+//
+//private:
+//    //Ui::ClientAppClass ui;
+//
+//    QLineEdit* clientIdInput;
+//    QLineEdit* levelInput;
+//    QLabel* outputLabel;
+//
+//    void sendMoveRequest(const std::string& direction);
+//};
