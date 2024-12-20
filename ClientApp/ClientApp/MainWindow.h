23fc6c013db2e9e1fe57d8a412765a7ca26f9dc4
Stoica Andra Stefania <andra.stoica@student.unitbv.ro>
2024-12-20 15:39:05 +0200

    testing the playerWindow in mainWindow and comment the existing code

--- ClientApp/ClientApp/MainWindow.h
+++ ClientApp/ClientApp/MainWindow.h
@@ -5,37 +5,37 @@
 #include <QWidget>
 #include <QLabel>
 #include <QPixmap>
-#include <QVBoxLayout>
-#include <QKeyEvent>
-#include <QMainWindow>
-//#include "ClientMap.h"
-
-
-#include "StartGame.h"
-#include "ClientApp.h"
-#include "ClientServer.h"
-
-class MainWindow : public QMainWindow //QWidget 
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget* parent = nullptr);
-    ~MainWindow();    
-   
-private slots:
-    void firstWindow();            //start game
-    void onStartGameClicked();     // log 
-
-private:
-
-    QLabel* imageLabel; // Imaginea de fundal
-    QLabel* textLabel;  // Textul "Start Game"
-    QPixmap pixmap;     // Imaginea încărcată
-   // ClientMap* map;
-};
-
-#endif // MAINWINDOW_H
+//#include <QVBoxLayout>
+//#include <QKeyEvent>
+//#include <QMainWindow>
+////#include "ClientMap.h"
+//
+//
+//#include "StartGame.h"
+//#include "ClientApp.h"
+//#include "ClientServer.h"
+//
+//class MainWindow : public QMainWindow //QWidget 
+//{
+//    Q_OBJECT
+//
+//public:
+//    explicit MainWindow(QWidget* parent = nullptr);
+//    ~MainWindow();    
+//   
+//private slots:
+//    void firstWindow();            //start game
+//    void onStartGameClicked();     // log 
+//
+//private:
+//
+//    QLabel* imageLabel; // Imaginea de fundal
+//    QLabel* textLabel;  // Textul "Start Game"
+//    QPixmap pixmap;     // Imaginea încărcată
+//   // ClientMap* map;
+//};
+//
+//#endif // MAINWINDOW_H
 
 
 //MainWindow pentru harta
@@ -59,3 +59,31 @@ private:
 //};
 //
 //#endif // MAINWINDOW_H
+
+
+//   TEST PLENTRU PLAYER 
+#pragma once
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include "PlayerWindow.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void OnDisplayButtonClicked();
+
+private:
+    QLineEdit* codeInput;
+    QPushButton* displayButton;
+    QLabel* imageLabel;
+    PlayerWindow playerWindow;
+};
