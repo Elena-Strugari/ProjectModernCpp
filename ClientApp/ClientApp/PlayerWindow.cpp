4ae5438f962604095c6f7f8f0705d696429229aa
Stoica Andra Stefania <andra.stoica@student.unitbv.ro>
2024-12-20 14:39:33 +0200

    created PlayerWindow class

---
+++ ClientApp/ClientApp/PlayerWindow.cpp
@@ -0,0 +1,28 @@
+﻿#include "PlayerWindow.h"
+
+PlayerWindow::PlayerWindow(QWidget* parent) : QWidget(parent), m_imageLabel(new QLabel(this))
+{
+	m_imageLabel->setAlignment(Qt::AlignCenter);
+	m_imageLabel->resize(400, 400);
+}
+void PlayerWindow::DisplayPlayerImage(int code)
+{
+	QString imagePath;
+	switch (code)
+	{
+
+	}
+	ShowImage(imagePath);
+}
+void PlayerWindow::ShowImage(const QString& imagePath)
+{
+	QPixmap pixmap(imagePath);
+
+	if (pixmap.isNull())
+	{
+		qDebug() << "Imaginea nu a putut fi încărcată: " << imagePath;
+		return;
+	}
+
+	m_imageLabel->setPixmap(pixmap.scaled(m_imageLabel->size(), Qt::KeepAspectRatio));
+}
