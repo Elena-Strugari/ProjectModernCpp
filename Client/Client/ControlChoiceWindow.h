5e6824de11df798d98a267654415f54f88cb8c03
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-12-30 18:18:26 +0200

    create header for ControlChoiseWindow

--- Client/Client/ControlChoiceWindow.h
+++ Client/Client/ControlChoiceWindow.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef CONTROLCHOICEWINDOW_H
+#define CONTROLCHOICEWINDOW_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QString>
+
+class ControlChoiceWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ControlChoiceWindow(const QString& clientId, QWidget* parent = nullptr);
+    ~ControlChoiceWindow();
+
+private:
+    QString clientId;
+    QPushButton* wasdButton;
+    QPushButton* arrowButton;
+
+    void initializeBackground();
+
+private slots:
+    void onWasdChosen();
+    void onArrowChosen();
+};
+
+#endif // CONTROLCHOICEWINDOW_H
