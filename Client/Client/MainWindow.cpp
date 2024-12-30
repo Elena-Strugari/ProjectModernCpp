05a4eac7f6217ec4ce6363b25800d8ede7b88914
DenisaStoica <denisa.stoica@student.unitbv.ro>
2024-12-30 18:00:41 +0200

    add image for directions and commented code for testing

--- Client/Client/MainWindow.cpp
+++ Client/Client/MainWindow.cpp
@@ -1,101 +1,101 @@
-﻿//#include "MainWindow.h"
-//#include "StartGameWindow.h"
-//#include "LoginWindow.h"
-//
-//
-////Reconstructia lui MainWindow
-//
-////Am inlocuit toate aparitiile lui QPixmap cu QPalette
-////Avem probleme la incarcarea din fisierul qrc a imaginilor...daca dam calea absoluta functioneaza.
-//
-//
-//MainWindow::MainWindow(QWidget* parent)
-//: QMainWindow(parent)
-//{
-//    qDebug() << "Current working directory:" << QDir::currentPath();
-//    qDebug() << "MainWindow: Constructor called.";
-//
-//    setWindowTitle("Main Window");
-//    resize(800, 600);
-//    firstWindow();
-//}
-//
-//void MainWindow::initializeBackground()
-//{
-//   // QImage testImage("D:\\Facultate\\Anul_2\\Semestrul_1\\Modern_C++\\ProjectModernCpp\\Client\\Client\\resources\\StartGame.jpg");
-//    QString resourcePath = ":/StartImage/resources/StartGame.jpg";
-//   //QString resourcePath = ":/PlayersImage/resources/Astronaut_1.png";
-//   // qDebug() << "Attempting to load image:" << resourcePath;
-//    QImage testImage(resourcePath);
-//
-//    if (testImage.isNull())
-//    {
-//        qDebug() << "Failed to load image from resource!";
-//    }
-//    else
-//    {
-//        qDebug() << "Image loaded successfully from resource!";
-//    }
-//
-//
-//    QPalette palette;
-//    palette.setBrush(QPalette::Window, QBrush(testImage.scaled(size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation)));
-//    setPalette(palette);
-//    setAutoFillBackground(true);
-//
-//}
-//
-//MainWindow::~MainWindow()
-//{
-//    //EMPTY
-//}
-//
-//
-//
-//void MainWindow::firstWindow()
-//{
-//    qDebug() << "Initializing StartGameWindow...";
-//    StartGameWindow* startGameWindow = new StartGameWindow(this);
-//    startGameWindow->initializeBackground();
-//    setCentralWidget(startGameWindow);
-//    connect(startGameWindow, &StartGameWindow::startGame, this, &MainWindow::onStartGameClicked);
-//
-//    qDebug() << "StartGameWindow set as central widget.";
-//}
-//
-//void MainWindow::onStartGameClicked()
-//{
-//    qDebug() << "Start Game action triggered!";
-//    LoginWindow* loginWindow = new LoginWindow();
-//    loginWindow->show();
-//    close(); 
-//}
+﻿#include "MainWindow.h"
+#include "StartGameWindow.h"
+#include "LoginWindow.h"
 
 
+//Reconstructia lui MainWindow
 
-//  TEST PENTRU PLAYER
+//Am inlocuit toate aparitiile lui QPixmap cu QPalette
+//Avem probleme la incarcarea din fisierul qrc a imaginilor...daca dam calea absoluta functioneaza.
 
-#include "MainWindow.h"
-#include <QVBoxLayout>
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent),
-    imageLabel(new QLabel(this))
+: QMainWindow(parent)
 {
-    qDebug() << "Entering MainWindow constructor";
-    QWidget* centralWidget = new QWidget(this);
-    QVBoxLayout* layout = new QVBoxLayout(centralWidget);
-    imageLabel->resize(150, 150);
-    imageLabel->setAlignment(Qt::AlignCenter);
-    layout->addWidget(imageLabel);
-    setCentralWidget(centralWidget);
-   //element.DisplayBonusLifeImage(imageLabel);
-   element.DisplayBombImage(imageLabel);
-   //element.DisplayBorderImage(imageLabel);
-   //element.DisplayWallImage(2, imageLabel);
-   //element.DisplayPlayerImage(44, imageLabel);
+    qDebug() << "Current working directory:" << QDir::currentPath();
+    qDebug() << "MainWindow: Constructor called.";
+
+    setWindowTitle("Main Window");
+    resize(800, 600);
+    firstWindow();
 }
 
-MainWindow::~MainWindow() = default;
+void MainWindow::initializeBackground()
+{
+    //QImage testImage("D:\\Facultate\\Anul_2\\Semestrul_1\\Modern_C++\\ProjectModernCpp\\Client\\Client\\resources\\StartGame.jpg");
+    QString resourcePath = ":/StartImage/resources/StartGame.jpg";
+    //QString resourcePath = ":/PlayersImage/resources/Astronaut_1.png";
+    //qDebug() << "Attempting to load image:" << resourcePath;
+    QImage testImage(resourcePath);
 
+    if (testImage.isNull())
+    {
+        qDebug() << "Failed to load image from resource!";
+    }
+    else
+    {
+        qDebug() << "Image loaded successfully from resource!";
+    }
+
+
+    QPalette palette;
+    palette.setBrush(QPalette::Window, QBrush(testImage.scaled(size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation)));
+    setPalette(palette);
+    setAutoFillBackground(true);
+
+}
+
+MainWindow::~MainWindow()
+{
+    //EMPTY
+}
+
+
+
+void MainWindow::firstWindow()
+{
+    qDebug() << "Initializing StartGameWindow...";
+    StartGameWindow* startGameWindow = new StartGameWindow(this);
+    startGameWindow->initializeBackground();
+    setCentralWidget(startGameWindow);
+    connect(startGameWindow, &StartGameWindow::startGame, this, &MainWindow::onStartGameClicked);
+
+    qDebug() << "StartGameWindow set as central widget.";
+}
+
+void MainWindow::onStartGameClicked()
+{
+    qDebug() << "Start Game action triggered!";
+    LoginWindow* loginWindow = new LoginWindow();
+    loginWindow->show();
+    close(); 
+}
+
+
+
+////  TEST PENTRU PLAYER
+//
+//#include "MainWindow.h"
+//#include <QVBoxLayout>
+//#include <QDebug>
+//
+//MainWindow::MainWindow(QWidget* parent)
+//    : QMainWindow(parent),
+//    imageLabel(new QLabel(this))
+//{
+//    qDebug() << "Entering MainWindow constructor";
+//    QWidget* centralWidget = new QWidget(this);
+//    QVBoxLayout* layout = new QVBoxLayout(centralWidget);
+//    imageLabel->resize(150, 150);
+//    imageLabel->setAlignment(Qt::AlignCenter);
+//    layout->addWidget(imageLabel);
+//    setCentralWidget(centralWidget);
+//   //element.DisplayBonusLifeImage(imageLabel);
+//   element.DisplayBombImage(imageLabel);
+//   //element.DisplayBorderImage(imageLabel);
+//   //element.DisplayWallImage(2, imageLabel);
+//   //element.DisplayPlayerImage(44, imageLabel);
+//}
+//
+//MainWindow::~MainWindow() = default;
+//
--- Client/Client/MainWindow.h
+++ Client/Client/MainWindow.h
@@ -1,43 +1,43 @@
-﻿//#ifndef MAINWINDOW_H
-//#define MAINWINDOW_H
-//#include <QVBoxLayout>
-//#include <QKeyEvent>
-//#include <QMainWindow>
-////#include "ClientMap.h"
-//#include <QFile>
-//
-//#include "StartGameWindow.h"
-//#include "Client.h"
-//#include "ClientServer.h"
-//#include <QDir>
-//
-//class MainWindow : public QMainWindow //QWidget 
-//{
-//    Q_OBJECT
-//
-//public:
-//    explicit MainWindow(QWidget* parent = nullptr);
-//    ~MainWindow();    
-//   
-//private slots:
-//    void firstWindow();            //start game
-//    void onStartGameClicked();     // log 
-//   // void onSignInClicked();
-//  //  void onSignUpClicked();
-//
-//private:
-//    void initializeBackground();
-//    QLabel* imageLabel = nullptr; // Imaginea de fundal
-//    QLabel* textLabel = nullptr;  // Textul "Start Game"
-//    //QPixmap backgroundPixmap;     // Imaginea încãrcatã
-//   // ClientMap* map;
-//};
-//
-//#endif // MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QVBoxLayout>
+#include <QKeyEvent>
+#include <QMainWindow>
+//#include "ClientMap.h"
+#include <QFile>
+
+#include "StartGameWindow.h"
+#include "Client.h"
+#include "ClientServer.h"
+#include <QDir>
+
+class MainWindow : public QMainWindow //QWidget 
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();    
+   
+private slots:
+    void firstWindow();            //start game
+    void onStartGameClicked();     // log 
+   // void onSignInClicked();
+  //  void onSignUpClicked();
+
+private:
+    void initializeBackground();
+    QLabel* imageLabel = nullptr; // Imaginea de fundal
+    QLabel* textLabel = nullptr;  // Textul "Start Game"
+    //QPixmap backgroundPixmap;     // Imaginea încãrcatã
+   // ClientMap* map;
+};
+
+#endif // MAINWINDOW_H
 
 
 //MainWindow pentru harta
-
+//
 //#pragma once
 //#ifndef MAINWINDOW_H
 //#define MAINWINDOW_H
@@ -58,26 +58,26 @@
 //
 //#endif // MAINWINDOW_H
 
-
-//   TEST PLENTRU PLAYER 
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QLabel>
-#include "DisplayElements.h" // Include clasa Element dacă este necesar
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget* parent = nullptr);
-    ~MainWindow();
-
-private:
-    QLabel* imageLabel;
-    DisplayElements element; // Instanța clasei care gestionează imaginile
-};
-
-#endif // MAINWINDOW_H
+//
+////   TEST PLENTRU PLAYER 
+//#ifndef MAINWINDOW_H
+//#define MAINWINDOW_H
+//
+//#include <QMainWindow>
+//#include <QLabel>
+//#include "DisplayElements.h" // Include clasa Element dacă este necesar
+//
+//class MainWindow : public QMainWindow
+//{
+//    Q_OBJECT
+//
+//public:
+//    explicit MainWindow(QWidget* parent = nullptr);
+//    ~MainWindow();
+//
+//private:
+//    QLabel* imageLabel;
+//    DisplayElements element; // Instanța clasei care gestionează imaginile
+//};
+//
+//#endif // MAINWINDOW_H
